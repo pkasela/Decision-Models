eefd81eb497b54412d3bfa3d3b6dbf49e9ab4752
pkasela <p.kasela@campus.unimib.it>
2019-04-19 01:41:22 +0200

    A2 mostly completed, maybe a review later

--- Assignment 2/Assigment2.R
+++ Assignment 2/Assigment2.R
@@ -1,6 +1,7 @@
 ######################################
 #####################################
 # Exercise c.
+#non binary model
 if(require(lpSolveAPI)==FALSE) install.packages("lpSolveAPI")
 model = make.lp(0,4)                        # 4 Variables (A,B,C,D)
 lp.control(model, sense = "max")            #Maximazing Problem
@@ -54,6 +55,116 @@ dimnames(model)<- list(c("Aluminium","Copper","Forger","Reamer","Shaper","Planer
                        c("A","B","C","D"))
 
 print(model) #To see to model
+
+
+#Binary model
+bin_model = make.lp(0,8)                    # 8 Variables
+set.type(bin_model, c(5:8), "binary")
+lp.control(bin_model, sense = "max")        #Maximazing Problem
+set.objfn(bin_model,obj = c(2.141,1.125,4,2.116,-600,-500,-250,-300))   #Objective function coefficients
+
+
+row.add.mode(bin_model,"on")
+
+add.constraint(bin_model,
+               xt = c(0.55,0.45,0.25,1),
+               type = "<=",
+               rhs = 1300,
+               indices = c(1:4)) #Aluminium Constraint
+
+add.constraint(bin_model,
+               xt = c(0.45,0.55,0.75,0),
+               type = "<=",
+               rhs = 900,
+               indices = c(1:4)) #Copper Constraint
+
+add.constraint(bin_model,
+               xt = c(1.2,1,1,1.5),
+               type = "<=",
+               rhs = 3600,
+               indices = c(1:4)) #Forger Time Constraint
+
+add.constraint(bin_model,
+               xt = c(1.75,1,1,1.5),
+               type = "<=",
+               rhs = 3600,
+               indices = c(1:4)) #Reamer Time Constraint
+
+add.constraint(bin_model,
+               xt = c(1.3,0.6,0.4,0),
+               type = "<=",
+               rhs = 3600,
+               indices = c(1:4)) #Shaper Time Constraint
+
+add.constraint(bin_model,
+               xt = c(2.5,1.2,2,1.5),
+               type = "<=",
+               rhs = 3600,
+               indices = c(1:4)) #Planer Time Constraint
+
+add.constraint(bin_model,
+               xt = c(1,-1440),
+               type = "<=",
+               rhs = 0,
+               indices = c(1,5)) #linking contraint for A
+
+add.constraint(bin_model,
+               xt = c(1,-1636.36),
+               type = "<=",
+               rhs = 0,
+               indices = c(2,6)) #linking contraint for B
+
+add.constraint(bin_model,
+               xt = c(1,-1200),
+               type = "<=",
+               rhs = 0,
+               indices = c(3,7)) #linking contraint for C
+
+add.constraint(bin_model,
+               xt = c(1,-1300),
+               type = "<=",
+               rhs = 0,
+               indices = c(4,8)) #linking contraint for D
+
+add.constraint(bin_model,
+               xt = c(-1,280.24),
+               type = "<=",
+               rhs = 0,
+               indices = c(1,5)) #min contraint for A
+
+add.constraint(bin_model,
+               xt = c(-1,444.44),
+               type = "<=",
+               rhs = 0,
+               indices = c(2,6)) #min contraint for B
+
+add.constraint(bin_model,
+               xt = c(-1,62.5),
+               type = "<=",
+               rhs = 0,
+               indices = c(3,7)) #min contraint for C
+
+add.constraint(bin_model,
+               xt = c(-1,141.78),
+               type = "<=",
+               rhs = 0,
+               indices = c(4,8)) #min contraint for D
+
+#Setting lower and upper bounds
+set.bounds(bin_model, lower = c(1200,450,0,0,0,0,0,0), 
+           upper = c(Inf,650,120,150,1,1,1,1)) 
+
+row.add.mode(bin_model,"off")
+
+#Giving names to the contraints and variables
+dimnames(bin_model)<- list(c("Aluminium","Copper","Forger","Reamer","Shaper","Planer",
+                             "Linking A","Linking B","Linking C","Linking D",
+                             "Min Bin A","Min Bin B","Min Bin C","Min Bin D"),
+                           c("A","B","C","D","Y_A","Y_B","Y_C","Y_D"))
+name.lp(bin_model, "Alloy Corporation Binary Problem")
+
+print(bin_model)
+
 
 
 ################################################
